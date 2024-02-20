84847b9e2f572404b3a39708876e3b4804e8578f
Jeff Hegeman <jeff@608.software>
2024-02-20 12:09:59 -0600

    Add Brave Browser option

--- init.sh
+++ init.sh
@@ -44,6 +44,19 @@ else
   echo "Skipping Discord."
 fi
 
+
+read -p "Do you want to install Brave Browser? (y/n) " install_brave
+
+if [[ $install_brave =~ ^[Yy]$ ]]; then
+  echo "Installing Brave Browser..."
+  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+  sudo apt update
+  sudo apt install brave-browser
+else
+  echo "Skipping Brave Browser."
+fi
+
 read -p "Do you want to adjust clock so it works with Windows dual boot? (y/n) " adjust_clock
 
 if [[ $adjust_clock =~ ^[Yy]$ ]]; then
